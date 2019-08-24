31f3dc4c80512086ccd10296d933917a88c818b0
“sai-dhanunjai” <111444333sai@gmail.com>
2019-08-24 15:04:13 +0530

    first edit of helloworld

--- hello.java
+++ hello.java
@@ -2,7 +2,10 @@ public class hello
 {
 	public static void main (String[] args)
 		{
-			System.out.println("Hello world");
+		  	for(int i=1;i<=5;i++)
+			{	
+				System.out.println("Hello world");
+			}
 
 		}
 }
